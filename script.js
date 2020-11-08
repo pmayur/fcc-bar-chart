56ba466b792d1fe661e4a99161a6da01f812e909
pmayur <thisispatil@gmail.com>
2020-11-08 21:02:14 +0530

    Utilizes Date Scale instead of Linear

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let max;
 
 // set the dimensions and margins of the graph
 var margin = { top: 30, right: 30, bottom: 70, left: 60 },
-    width = 5000 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = 900 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
 var svg = d3
     .select("#root")
@@ -16,28 +16,32 @@ var svg = d3
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json(source).then(function (data) {
-    max = data.data[0][1];
-    data.data.forEach((element) => {
-        console.log(element[0], ":", element[1]);
-    });
-});
-
 d3.json(source).then((data) => {
+    var yearsDate = data.data.map(function (item) {
+        return new Date(item[0]);
+    });
+
+    var xMax = new Date(d3.max(yearsDate));
+    xMax.setMonth(xMax.getMonth() + 3);
+    var xScale = d3
+        .scaleTime()
+        .domain([d3.min(yearsDate), xMax])
+        .range([0, width]);
+
     var x = d3
         .scaleBand()
         .range([0, width])
         .domain(
-            data.data.map(function (d) {
+            data.data.map(function (d, i) {
                 return d[0];
             })
         );
 
     // x axis
     svg.append("g")
-        .attr("id", "x-axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
+        .attr("id", "x-axis")
+        .call(d3.axisBottom(xScale))
         .selectAll("text")
         .attr("transform", "translate(-10,0)rotate(-45)")
         .style("text-anchor", "end");
@@ -51,11 +55,11 @@ d3.json(source).then((data) => {
         .enter()
         .append("rect")
         .attr("class", "bar")
-        .attr("x", function (d) {
-            return x(d[0]);
-        })
         .attr("data-date", function (d) {
             return d[0];
+        })
+        .attr("x", function (d) {
+            return x(d[0]);
         })
         .attr("y", function (d) {
             return y(d[1]);
