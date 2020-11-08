b8312ff4801448b495db281a87d27c89872dfcef
pmayur <thisispatil@gmail.com>
2020-11-08 16:35:40 +0530

    Displays Bar Chart

--- script.js
+++ script.js
@@ -1,58 +1,71 @@
-var csvSource =
-    "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv";
+var source =
+    "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+
+let max;
 
 // set the dimensions and margins of the graph
 var margin = { top: 30, right: 30, bottom: 70, left: 60 },
-    width = 460 - margin.left - margin.right,
+    width = 5000 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
 var svg = d3
-    .select("#my_dataviz")
+    .select("#root")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Parse the Data
-d3.csv(csvSource).then(function (data) {
-    // X axis
+d3.json(source).then(function (data) {
+    max = data.data[0][1];
+    data.data.forEach((element) => {
+        console.log(element[0], ":", element[1]);
+    });
+});
+
+d3.json(source).then((data) => {
     var x = d3
         .scaleBand()
         .range([0, width])
         .domain(
-            data.map(function (d) {
-                return d.Country;
+            data.data.map(function (d) {
+                return d[0];
             })
-        )
-        .padding(0.2);
+        );
 
+    // x axis
     svg.append("g")
+        .attr("id", "x-axis")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x))
         .selectAll("text")
         .attr("transform", "translate(-10,0)rotate(-45)")
         .style("text-anchor", "end");
 
-    // Add Y axis
-    var y = d3.scaleLinear().domain([0, 13000]).range([height, 0]);
-    svg.append("g").call(d3.axisLeft(y));
+    var y = d3.scaleLinear().domain([0, 20000]).range([height, 10]);
 
-    // Bars
+    svg.append("g").attr("id", "y-axis").call(d3.axisLeft(y));
+
     svg.selectAll("mybar")
-        .data(data)
+        .data(data.data)
         .enter()
         .append("rect")
+        .attr("class", "bar")
         .attr("x", function (d) {
-            return x(d.Country);
+            return x(d[0]);
+        })
+        .attr("data-date", function (d) {
+            return d[0];
         })
         .attr("y", function (d) {
-            return y(d.Value);
+            return y(d[1]);
+        })
+        .attr("data-gdp", function (d) {
+            return d[1];
         })
         .attr("width", x.bandwidth())
         .attr("height", function (d) {
-            return height - y(d.Value);
+            return height - y(d[1]);
         })
         .attr("fill", "#69b3a2");
 });
